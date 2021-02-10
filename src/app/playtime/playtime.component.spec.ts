4259d6854f92bdff33a6e66c49dc8b4f06c02434
Allan Kanyemba <allankanyemba@gmail.com>
2021-02-11 01:36:59 +0200

    Angular update from 10.0.2 to 11.x.x

--- src/app/playtime/playtime.component.spec.ts
+++ src/app/playtime/playtime.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PlaytimeComponent } from './playtime.component';
 
@@ -6,7 +6,7 @@ describe('PlaytimeComponent', () => {
   let component: PlaytimeComponent;
   let fixture: ComponentFixture<PlaytimeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PlaytimeComponent ]
     })
