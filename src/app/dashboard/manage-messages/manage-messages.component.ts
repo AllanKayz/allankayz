cd61436bde6c0cab87b941e4da14134712bb8e0f
Allan Kanyemba <allankanyemba@gmail.com>
2021-02-14 05:59:40 +0200

    Editing messages section

--- src/app/dashboard/manage-messages/manage-messages.component.ts
+++ src/app/dashboard/manage-messages/manage-messages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faCalendarDay, faEnvelope, faPen, faPlus, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-manage-messages',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-messages.component.css']
 })
 export class ManageMessagesComponent implements OnInit {
-
+  // Get Font Awesome Icons
+  faCalendarDay = faCalendarDay;
+  faEnvelope = faEnvelope;
+  faPlus = faPlus;
+  faPen = faPen;
+  faTrashAlt = faTrashAlt;
+  // current date variable
+  currentDate;
+  // Number of new messages
+  newMessages = 6;
   constructor() { }
 
   ngOnInit(): void {
+    const dateOb: Date = new Date();
+    // adjust 0 single digit date
+    const date = ('0' + dateOb.getDate()).slice(-2);
+    // current month
+    const month = ('0' + (dateOb.getMonth() + 1)).slice(-2);
+    const year = dateOb.getFullYear();
+    const zuva = year + ',' + month + ',' + date;
+    const zuvax = new Date(zuva);
+    // date to human friendly readable format
+    const mnth = zuvax.toLocaleString('default', { month: 'long' });
+    const chizuva = date + ' ' + mnth + ' ' + year;
+    this.currentDate = chizuva;
   }
-
 }
