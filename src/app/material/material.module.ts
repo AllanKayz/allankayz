2fac1f260634d455dfa5c90e89a0be10d163ae05
Allan Kayz <allankanyemba@gmail.com>
2021-10-17 23:07:25 +0200

    Space in comments issue fixed

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,79 +1,79 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-//import { MatAutocompleteModule } from '@angular/material/autocomplete';
+// import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatBadgeModule } from '@angular/material/badge';
-//import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+// import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
-//import { MatCheckboxModule } from '@angular/material/checkbox';
-//import { MatChipsModule } from '@angular/material/chips';
-//import { MatDatepickerModule } from '@angular/material/datepicker';
-//import { MatDialogModule } from '@angular/material/dialog';
+// import { MatCheckboxModule } from '@angular/material/checkbox';
+// import { MatChipsModule } from '@angular/material/chips';
+// import { MatDatepickerModule } from '@angular/material/datepicker';
+// import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
-//import { MatExpansionModule } from '@angular/material/expansion';
+// import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-//import { MatListModule } from '@angular/material/list';
+// import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
-//import { MatPaginatorModule } from '@angular/material/paginator';
+// import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatToolbarModule } from '@angular/material/toolbar';
-//import { MatTooltipModule } from '@angular/material/tooltip';
-//import { MatTreeModule } from '@angular/material/tree';
-//import { MatTableModule } from '@angular/material/table';
-//import { MatStepperModule } from '@angular/material/stepper';
-//import { MatSortModule } from '@angular/material/sort';
-//import { MatSnackBarModule } from '@angular/material/snack-bar';
-//import { MatSliderModule } from '@angular/material/slider';
-//import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-//import { MatSidenavModule } from '@angular/material/sidenav';
-//import { MatSelectModule } from '@angular/material/select';
-//import { MatRadioModule } from '@angular/material/radio';
+// import { MatTooltipModule } from '@angular/material/tooltip';
+// import { MatTreeModule } from '@angular/material/tree';
+// import { MatTableModule } from '@angular/material/table';
+// import { MatStepperModule } from '@angular/material/stepper';
+// import { MatSortModule } from '@angular/material/sort';
+// import { MatSnackBarModule } from '@angular/material/snack-bar';
+// import { MatSliderModule } from '@angular/material/slider';
+// import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+// import { MatSidenavModule } from '@angular/material/sidenav';
+// import { MatSelectModule } from '@angular/material/select';
+// import { MatRadioModule } from '@angular/material/radio';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-//import { MatNativeDateModule } from '@angular/material/core';
-//import { MatTabsModule } from '@angular/material/tabs';
+// import { MatNativeDateModule } from '@angular/material/core';
+// import { MatTabsModule } from '@angular/material/tabs';
 
 
 const MaterialComponents = [
-  //MatAutocompleteModule,
+  // MatAutocompleteModule,
   MatBadgeModule,
-  //MatBottomSheetModule,
+  // MatBottomSheetModule,
   MatButtonModule,
   MatCardModule,
-  //MatCheckboxModule,
-  //MatChipsModule,
-  //MatDatepickerModule,
-  //MatDialogModule,
+  // MatCheckboxModule,
+  // MatChipsModule,
+  // MatDatepickerModule,
+  // MatDialogModule,
   MatDividerModule,
-  //MatExpansionModule,
+  // MatExpansionModule,
   MatGridListModule,
   MatFormFieldModule,
   MatIconModule,
   MatInputModule,
-  //MatListModule,
+  // MatListModule,
   MatMenuModule,
-  //MatPaginatorModule,
+  // MatPaginatorModule,
   MatToolbarModule,
-  //MatTooltipModule,
-  //MatTreeModule,
-  //MatTableModule,
-  //MatStepperModule,
-  //MatSortModule,
-  //MatSnackBarModule,
-  //MatSliderModule,
-  //MatSlideToggleModule,
-  //MatSlideToggleModule,
-  //MatSidenavModule,
-  //MatSelectModule,
-  //MatRadioModule,
+  // MatTooltipModule,
+  // MatTreeModule,
+  // MatTableModule,
+  // MatStepperModule,
+  // MatSortModule,
+  // MatSnackBarModule,
+  // MatSliderModule,
+  // MatSlideToggleModule,
+  // MatSlideToggleModule,
+  // MatSidenavModule,
+  // MatSelectModule,
+  // MatRadioModule,
   MatProgressSpinnerModule,
   MatProgressBarModule,
   MatButtonToggleModule,
-  //MatNativeDateModule,
-  //MatTabsModule
+  // MatNativeDateModule,
+  // MatTabsModule
 ]
 
 
